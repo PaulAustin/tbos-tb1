27c42e6819701a5f3d06e678ec8d6ee556916589
PaulAustin <canoetime@gmail.com>
2018-06-07 18:32:51 -0400

    Converting encoder structs to more C++ style class

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -26,85 +26,87 @@ SOFTWARE.
 
 EncoderManager gEncoders;
 
+//------------------------------------------------------------------
+void Encoder::Init() {
+	gRMap.SetValueObj(kRM_Motor1Encoder, &_count);
+	gRMap.SetValueObj(kRM_Motor1Rpm, &_rpm);
+	_ppr.Set(1400);
+	_count.Set(0);
+	_rpm.Set(0);
+}
 
-void EncoderManager::Init(void)
-{
-	gRMap.SetValueObj(kRM_Motor1Encoder, &_encoders[kENCODER_1]._count);
-	gRMap.SetValueObj(kRM_Motor1Rpm, &_encoders[kENCODER_1]._rpm);
-
-	gRMap.SetValueObj(kRM_Motor2Encoder, &_encoders[kENCODER_2]._count);
-	gRMap.SetValueObj(kRM_Motor2Rpm, &_encoders[kENCODER_2]._rpm);
-
-	for (int i = 0; i < kENCODER_Count; i++) {
-		_encoders[i]._ppr.Set(1400);
-		_encoders[i]._count.Set(0);
-		_encoders[i]._rpm.Set(0);
+//------------------------------------------------------------------
+void Encoder::Run() {
+	if (_count.HasAsyncSet()) {
+		// If register was written to then update the low level value.
+		// this is typically used to reset the counter.
+		_countEdge = _count.Get();
+	} else {
+		// Otherwise copy level counter to register.
+		_count.Set(_countEdge);
 	}
 }
 
-/*----------------------------------------------------------------------------
-Name: Encoder_RunISR
-Desc: Motor/Encoder State Machine
-	  Called at highest rate possible to poll encoder edges and
-	  to generate pulse chain for power control.
-/ ---------------------------------------------------------------------------*/
+//------------------------------------------------------------------
+void Encoder::CalcRPM() {
+	int current = _count.Get();
+	int rpm = ( current - _lastCount) * 10 / _ppr.Get();
+	_rpm.Set(rpm);
+	_lastCount = current;
+}
+
+
+//------------------------------------------------------------------
+void EncoderManager::Init(void) {
+	_e1.Init();
+	_e2.Init();
+}
+
+//------------------------------------------------------------------
+// Name: Encoder_RunISR
+// Desc: Motor/Encoder State Machine
+//	  Called at highest rate possible to poll encoder edges and
+//	  to generate pulse chain for power control.
+//------------------------------------------------------------------
 void EncoderManager::RunISR()
 {
 	int32_t delta = 0;
 
 	bool qA = GPIO_READ(gpio_ENC1_QA);
 	bool qB = GPIO_READ(gpio_ENC1_QB);
-	if (_encoders[kENCODER_1]._lastEdgeA != qA) {
-		_encoders[kENCODER_1]._lastEdgeA = qA;
+	if (_e1._lastEdgeA != qA) {
+		_e1._lastEdgeA = qA;
 		if (qA) {
 			delta = qB ? 1 : -1; 	// Rising edge on A
 		} else {
 			delta = qB ? -1 : 1; 	// Falling edge on A
 		}
-		_encoders[kENCODER_1]._countEdge += delta;
+		_e1._countEdge += delta;
 	}
 
 	qA = GPIO_READ(gpio_ENC2_QA);
 	qB = GPIO_READ(gpio_ENC2_QB);
-	if (_encoders[kENCODER_2]._lastEdgeA != qA) {
-		_encoders[kENCODER_2]._lastEdgeA = qA;
+	if (_e2._lastEdgeA != qA) {
+		_e2._lastEdgeA = qA;
 		if (qA) {
 			delta = qB ? 1 : -1; 	// Rising edge on A
 		} else {
 			delta = qB ? -1 : 1; 	// Falling edge on A
 		}
-		_encoders[kENCODER_2]._countEdge += delta;
+		_e2._countEdge += delta;
 	}
 }
 
-/*----------------------------------------------------------------------------
-Name: Encoder_Run
-/ ---------------------------------------------------------------------------*/
-void EncoderManager::Run(void)
-{
-	for (int i = 0; i < kENCODER_Count; i++) {
-		if (_encoders[i]._count.HasAsyncSet()) {
-			// If register was written to then update the low level value.
-			// this is typically used to reset the counter.
-			_encoders[kENCODER_1]._countEdge = _encoders[kENCODER_1]._count.Get();
-		} else {
-			// Other wise copy level counter to register.
-			_encoders[kENCODER_1]._count.Set(_encoders[kENCODER_1]._countEdge);
-		}
-	}
+//------------------------------------------------------------------
+void EncoderManager::Run(void) {
+	_e1.Run();
+	_e2.Run();
 }
 
-/*------------------------------------------------------------------
-/ ----------------------------------------------------------------*/
+//------------------------------------------------------------------
 // TODO: not done/tested
-void EncoderManager::CalckRPM(int dt)
-{
-	for (int i = kENCODER_1; i <= kENCODER_2; i++) {
-		Encoder* pE = &_encoders[i];
-		int current = pE->_count.Get();
-		int rpm = ( current - pE->_lastCount) * 10 / pE->_ppr.Get();
-		pE->_rpm.Set(rpm);
-		pE->_lastCount = current;
-	}
+void EncoderManager::CalckRPM() {
+	_e1.CalcRPM();
+	_e2.CalcRPM();
 }
 
