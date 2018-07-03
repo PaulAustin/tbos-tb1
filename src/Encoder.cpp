22267d158a989a9dd4546390daba0375ab6d284b
PaulAustin <canoetime@gmail.com>
2018-07-03 11:51:11 -0500

    Encoder back to classes
    fixed overwrite

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -27,19 +27,42 @@ SOFTWARE.
 EncoderManager gEncoders;
 
 
+//------------------------------------------------------------------
+void Encoder::Init() {
+	_ppr.Set(1400);
+	_count.Set(0);
+	_rpm.Set(0);
+}
+
+//------------------------------------------------------------------
+void Encoder::Run() {
+	if (_count.HasAsyncSet()) {
+		// If register was written to then update the low level value.
+		// this is typically used to reset the counter.
+		_countEdge = _count.Get();
+	} else {
+		// Otherwise copy level counter to register.
+		_count.Set(_countEdge);
+	}
+}
+
+//------------------------------------------------------------------
+void Encoder::CalcRPM() {
+	int current = _count.Get();
+	int rpm = ( current - _lastCount) * 10 / _ppr.Get();
+	_rpm.Set(rpm);
+	_lastCount = current;
+}
+
 void EncoderManager::Init(void)
 {
-	gRMap.SetValueObj(kRM_Motor1Encoder, &_encoders[kENCODER_1]._count);
-	gRMap.SetValueObj(kRM_Motor1Rpm, &_encoders[kENCODER_1]._rpm);
+	gRMap.SetValueObj(kRM_Motor1Encoder, &_e1._count);
+	gRMap.SetValueObj(kRM_Motor1Rpm, &_e1._rpm);
+	gRMap.SetValueObj(kRM_Motor1Cpr, &_e1._ppr);
 
-	gRMap.SetValueObj(kRM_Motor2Encoder, &_encoders[kENCODER_2]._count);
-	gRMap.SetValueObj(kRM_Motor2Rpm, &_encoders[kENCODER_2]._rpm);
-
-	for (int i = 0; i < kENCODER_Count; i++) {
-		_encoders[i]._ppr.Set(1400);
-		_encoders[i]._count.Set(0);
-		_encoders[i]._rpm.Set(0);
-	}
+	gRMap.SetValueObj(kRM_Motor2Encoder, &_e2._count);
+	gRMap.SetValueObj(kRM_Motor2Rpm, &_e2._rpm);
+	gRMap.SetValueObj(kRM_Motor2Cpr, &_e2._ppr);
 }
 
 /*----------------------------------------------------------------------------
@@ -54,26 +77,27 @@ void EncoderManager::RunISR()
 
 	bool qA = GPIO_READ(gpio_ENC1_QA);
 	bool qB = GPIO_READ(gpio_ENC1_QB);
-	if (_encoders[kENCODER_1]._lastEdgeA != qA) {
-		_encoders[kENCODER_1]._lastEdgeA = qA;
+	if (_e1._lastEdgeA != qA) {
+		_e1._lastEdgeA = qA;
 		if (qA) {
 			delta = qB ? 1 : -1; 	// Rising edge on A
 		} else {
 			delta = qB ? -1 : 1; 	// Falling edge on A
 		}
-		_encoders[kENCODER_1]._countEdge += delta;
+		_e1._countEdge += delta;
 	}
 
 	qA = GPIO_READ(gpio_ENC2_QA);
 	qB = GPIO_READ(gpio_ENC2_QB);
-	if (_encoders[kENCODER_2]._lastEdgeA != qA) {
-		_encoders[kENCODER_2]._lastEdgeA = qA;
+
+	if (_e2._lastEdgeA != qA) {
+		_e2._lastEdgeA = qA;
 		if (qA) {
 			delta = qB ? 1 : -1; 	// Rising edge on A
 		} else {
 			delta = qB ? -1 : 1; 	// Falling edge on A
 		}
-		_encoders[kENCODER_2]._countEdge += delta;
+		_e2._countEdge += delta;
 	}
 }
 
@@ -82,30 +106,16 @@ Name: Encoder_Run
 / ---------------------------------------------------------------------------*/
 void EncoderManager::Run(void)
 {
-	for (int i = 0; i < kENCODER_Count; i++) {
-		if (_encoders[i]._count.HasAsyncSet()) {
-			// If register was written to then update the low level value.
-			// this is typically used to reset the counter.
-			_encoders[kENCODER_1]._countEdge = _encoders[kENCODER_1]._count.Get();
-		} else {
-			// Other wise copy level counter to register.
-			_encoders[kENCODER_1]._count.Set(_encoders[kENCODER_1]._countEdge);
-		}
-	}
+	_e1.Run();
+	_e2.Run();
 }
 
 /*------------------------------------------------------------------
 / ----------------------------------------------------------------*/
 // TODO: not done/tested
-//What does 'dt' stand for??
-void EncoderManager::CalckRPM(int dt)
+void EncoderManager::CalckRPM()
 {
-	for (int i = kENCODER_1; i <= kENCODER_2; i++) {
-		Encoder* pE = &_encoders[i];
-		int current = pE->_count.Get();
-		int rpm = ( current - pE->_lastCount) * 10 / pE->_ppr.Get();
-		pE->_rpm.Set(rpm);
-		pE->_lastCount = current;
-	}
+	_e1.CalcRPM();
+	_e2.CalcRPM();
 }
 
