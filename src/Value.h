ad0e039cbcb488bcadcc29f37ed8fb57f2cf608a
PaulAustin <canoetime@gmail.com>
2018-12-07 11:55:08 -0600

    Rework Value class
    merged branches

--- src/Value.h
+++ src/Value.h
@@ -1,17 +1,3 @@
-/* basic classes for working with 'smart values. Not that smart really but a bit of
- * a standalone behaviour
- *
- *
- */
-
-
-#ifndef VALUE_H_
-#define VALUE_H_
-
-#define COUNT_OF(x) ((int)(sizeof(x)/sizeof(0[x])))
-
-// Async Values - designed to be set in an ISR and read by a fore ground process
-// In the ISR there is always a 'value' set. The symantics of setting that value
 /*
 Copyright (c) 2018 Trashbots, Inc. - SDG
 
@@ -34,69 +20,61 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#ifndef VALUE_H_
+#define VALUE_H_
 
-class ATrigger {
-protected:
-	int _aValue;
-public:
-	ATrigger() {
-		_aValue = 0;
-	}
-	virtual ~ATrigger() {};
-	virtual void ASet(int) {
-		// Ignore the value, just mark as set.
-		_aValue = true;
-	}
-	virtual int  Get() {
-		return false;
-	}
-	virtual bool HasAsyncSet() {
-		bool n = _aValue;
-		_aValue = false;
-		return n;
-	}
-	virtual void Reset() {
-		_aValue = false;
-	}
-};
+#define COUNT_OF(x) ((int)(sizeof(x)/sizeof(0[x])))
 
-class AValue : public ATrigger {
+class Value {
 protected:
 	int _value;
+	int _newValue;
 public:
-	AValue() {
-		_value = 0;
+	Value() {
+		_value = _newValue = 0;
 	}
-	virtual ~AValue() {};
+	virtual ~Value() {
 
-	virtual void ASet(int value) {
-		_aValue = value;
 	}
 	virtual void Set(int value) {
-		_aValue = value;
-		_value = value;
+		_newValue =  value;
+		_value =  value;
+	}
+	virtual void AsyncSet(int value) {
+		_newValue =  value;
 	}
 	virtual int  Get() {
 		return _value;
 	}
 	virtual bool HasAsyncSet() {
-		bool n = (_value != _aValue);
-		_value = _aValue;
-		return n;
-	}
-	virtual void Reset() {
-		_value = _aValue;
+		bool b = (_value != _newValue);
+		_value = _newValue;
+		return b;
 	}
 };
 
-class SlewValue : AValue {
+class TriggerValue : Value {
+//	virtual void Set(int)					{ _newValue = 1; }
 };
-class LinMapValue : AValue {
+
+
+class SlewValue : Value {
+
 };
-class OscValue : AValue {
+
+
+class LinMapValue : Value {
+
 };
-class PIPValue : AValue {
+
+class OscValue : Value {
+
 };
+
+class PIPValue : Value {
+
+};
+
 
 // EVF8  Escaped value format 8 byte chunks.
 enum {
@@ -133,13 +111,56 @@ enum {
 	EVT8_Int64,				// four bytes following
 	EVT8_Float64,			// four bytes following
 
-
 	// Null-byte terminated String
 	EVT8_UTF8  = 121,		// null terminated UTF8 string
 	// Counted Array ( all should be alike )
 	EVT8_EltList  = 122,	// counted list of elements
 	EVT8_NVPList  = 123,	// counted list of name value pairs
-
-	EVT8_GetReg   = 124,	//Number to get the register
 };
+
+/* Read and build ale a byte at a time. */
+class ValueReader {
+private:
+public:
+	int _v;
+	int _out;
+	int _bytesRemaining;
+
+	// Add reader proc for specific packet types?
+	// this replaces the a switch with a function call
+	// the sate proc can be
+	// in FPG it could be parallel logic all looking for finish clause.
+public:
+	ValueReader() {
+		Reset();
+	}
+	bool ReadV8(int ev8) {
+		if (_bytesRemaining == 0) {
+			if (ev8 >= EVT8_IntMiniMin && ev8 <= EVT8_MultiByteBase) {
+				_v = ev8;
+				return true;
+			} else if (ev8 == EVT8_Int16) {
+				_v = 0;
+				_bytesRemaining = 2;
+				return false;
+			} else if (ev8 == EVT8_Int32) {
+				_v = ev8;
+			} else if (ev8 == EVT8_UTF8) {
+				_v = ev8;
+			}
+		} else if (_bytesRemaining > 0) {
+			_v = (_v << 8) | (ev8 & 0x00ff);
+			_bytesRemaining--;
+			return (_bytesRemaining == 0);
+		}
+		return false;
+	}
+	int Value() { return _v; }
+	void Reset() {
+		_v = _bytesRemaining = 0;
+	}
+};
+
+
+
 #endif // VALUE_H_
